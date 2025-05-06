760b791ed858f09029cbaf036bcc33e26a1e60e3
rrixh <136478654+rrixh@users.noreply.github.com>
2025-05-05 22:16:46 -0500

    Update webimport.lua

--- webimport.lua
+++ webimport.lua
@@ -11,6 +11,7 @@ getgenv().folder=folder;
 
 getgenv().webimport = function(file,isfolder,arg3)
 if(owner==nil)then owner="rrixh" end;
+if(file==file..".uwuware")then repo = "uwuware" end;
 if(repo==nil)then warn("MUST ASSIGN VARIABLE --> Example:\n local repo = \"RepositoryNameHere\"") 
 return end;
 if(branch==nil)or(branch~="main")or(branch~="refs/heads/main")then
